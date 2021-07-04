496f173cc3f8b781fc1e846b0e856ed9dd5dca2d
sky-kt <skyler.kt@protonmail.com>
2021-07-04 15:01:13 -0700

    images can be switched

--- slider.js
+++ slider.js
@@ -1,14 +1,46 @@
 const leftArrowButton = document.getElementById('leftArrow')
 const rightArrowButton = document.getElementById('rightArrow')
+const focusedImage = document.getElementById('focusedImage')
 const imageArray = ['fsociety', 'forest', 'windows11', 'japan']
 
 let imageIndex = 0
+
+const loadImage = (idx) => {
+  const desiredImage = imageArray[idx]
+  let imageURL
+
+  switch (desiredImage) {
+    case 'fsociety':
+      console.log('fsociety')
+      imageURL = './images/fsociety.jpg'
+      break
+    case 'forest':
+      console.log('forest')
+      imageURL = './images/forest.jpeg'
+      break
+    case 'windows11':
+      console.log('windows11')
+      imageURL = './images/windows11.jpg'
+      break
+    case 'japan':
+      console.log('japan')
+      imageURL = './images/samurai.png'
+      break
+    default:
+      alert(`Image at index ${idx} does not exist`)
+      break
+  }
+  focusedImage.style.backgroundImage = `url(${imageURL})`
+}
+
+loadImage(0)
 
 leftArrowButton.addEventListener('click', () => {
   if ((imageIndex - 1) < 0) {
     imageIndex = imageArray.length - 1
   } else imageIndex--
   console.log(imageIndex)
+  loadImage(imageIndex)
 })
 
 rightArrowButton.addEventListener('click', () => {
@@ -16,4 +48,5 @@ rightArrowButton.addEventListener('click', () => {
     imageIndex = 0
   } else imageIndex++
   console.log(imageIndex)
+  loadImage(imageIndex)
 })
